53769f9c646e56fa3eae5db672086fc1f0d9dca2
WADR Umang M. Patel <umangmp@webline.local>
2019-01-03 17:02:26 +0530

    Changes for listners

--- application/components/ToDoListContainer.js
+++ application/components/ToDoListContainer.js
@@ -15,106 +15,131 @@ var config = {
 firebase.initializeApp(config);
 
 const databaseRef = (ref) => {
-    if (ref) {
-        return firebase.database().ref(ref)
-    } else {
-        return firebase.database().ref()
-    }
+	if (ref) {
+		return firebase.database().ref(ref)
+	} else {
+		return firebase.database().ref()
+	}
 }
 // Get a reference to the database service
 
 class ToDoContainer extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            items: []
-        };
-        // this.databaseRef = firebase.database().ref();
-        _this = this;
-        this.alertMenu = this.alertMenu.bind(this);
-        this.deleteItem = this.deleteItem.bind(this);
-        this.updateItem = this.updateItem.bind(this);
-        this.openItem = this.openItem.bind(this);
-        this.getItems = this.getItems.bind(this);
-    }
+	constructor() {
+		super();
+		this.state = {
+			items: []
+		};
+		// this.databaseRef = firebase.database().ref();
+		_this = this;
+		this.alertMenu = this.alertMenu.bind(this);
+		this.deleteItem = this.deleteItem.bind(this);
+		this.updateItem = this.updateItem.bind(this);
+		this.openItem = this.openItem.bind(this);
+		this.getItemsFromDatabase = this.getItemsFromDatabase.bind(this);
+	}
 
-    getItems() {
-        databaseRef('notes').orderByChild('createdAt').once('value', (snap) => {
-            this.setState({ items: snap.val() });
-        });
-    }
+	getItemsFromDatabase() {
+		/** Getting values from database */
+		// databaseRef('notes').orderByChild('createdAt').once('value', (snap) => {
+		// 	if (snap.exists()) {
+		// 		this.setState({ items: snap.val() });
+		// 	} else {
+		// 		this.setState({ items: [] });
+		// 	}
+		// });
 
-    componentWillMount() {
-        this.getItems()
-    }
+		// /* Child Changed Event*/
+		// databaseRef('notes').on('child_changed', (snap) => {
+		// 	// alert(snap.val())
+		// 	if(snap.val().txt == 'Umang'){
+		// 		alert("Event Occurs")
+		// 	}
+		// });
 
-    alertMenu(rowData, rowID) {
-        AlertIOS.alert(
-            'Quick Menu',
-            null,
-            [
-                { text: 'Delete', onPress: () => this.deleteItem(rowData, rowID) },
-                { text: 'Edit', onPress: () => this.openItem(rowData, rowID) },
-                { text: 'Cancel' }
-            ]
-        )
-    }
+		/* Listener for value changes */
+		databaseRef('notes').orderByChild("txt").on('value', (snap) => {
+			let tempArray = []
+			// console.log("--> ",snap.val());
+			snap.forEach(function (childSnapshot) {
+				// console.log("--> ",childSnapshot.val());
+				if (childSnapshot.exists()) {
+					tempArray.push(childSnapshot.val())
+					}
+			})
+			this.setState({ items: tempArray });
+		});
 
-    deleteItem(data, index) {
-        console.log("-___---->", data);
+	}
 
-        databaseRef('notes').child(data.id).remove().then(function () {
-            _this.getItems()
-            alert("Remove succeeded.")
-        })
-            .catch(function (error) {
-                alert("Remove failed: " + error.message)
-            });
-    }
+	componentWillMount() {
+		this.getItemsFromDatabase()
+	}
 
-    updateItem(item, index, instance) {
-        databaseRef('notes/').child(item.id).set({
-            txt: item.txt,
-            id: item.id,
-            createdAt: item.createdAt,
-            complete: item.complete
-        });
-        this.getItems()
-        instance.props.navigation.goBack();
-    }
+	alertMenu(rowData, rowID) {
+		AlertIOS.alert(
+			'Quick Menu',
+			null,
+			[
+				{ text: 'Delete', onPress: () => this.deleteItem(rowData, rowID) },
+				{ text: 'Edit', onPress: () => this.openItem(rowData, rowID) },
+				{ text: 'Cancel' }
+			]
+		)
+	}
 
-    openItem(rowData, rowID) {
-        this.props.navigation.navigate('ToDoEdit', {
-            item: rowData,
-            id: rowID,
-            update: this.updateItem
-        })
-    }
+	deleteItem(data, index) {
+		console.log("-___---->", data);
+		databaseRef('notes').child(data.id).remove().then(function () {
+			// _this.getItemsFromDatabase()
+			alert("Remove succeeded.")
+		}).catch(function (error) {
+			alert("Remove failed: " + error.message)
+		});
+	}
 
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-                {
-                    (this.state.items.length <= 0) ?
-                        <View style={styles.mainView}>
-                            <Text style={styles.noDataText}>No Data Available</Text>
-                        </View>
-                        :
-                        <ToDoList
-                            items={this.state.items}
-                            onPressItem={this.openItem}
-                            onLongPressItem={this.alertMenu} />
-                }
+	updateItem(item, index, instance) {
+		databaseRef('notes/').child(item.id).set({
+			txt: item.txt,
+			id: item.id,
+			createdAt: item.createdAt,
+			complete: item.complete
+		});
+		// this.getItemsFromDatabase()
+		instance.props.navigation.goBack();
+	}
 
-                <TouchableHighlight
-                    style={[styles.button, styles.newButton]}
-                    underlayColor='#99d9f4'
-                    onPress={this.openItem}>
-                    <Text style={styles.buttonText}>+</Text>
-                </TouchableHighlight>
-            </View>
-        );
-    }
+	openItem(rowData, rowID) {
+		this.props.navigation.navigate('ToDoEdit', {
+			item: rowData,
+			id: rowID,
+			update: this.updateItem
+		})
+	}
+
+	render() {
+		return (
+			<View style={{ flex: 1 }}>
+				{
+					(this.state.items.length <= 0) ?
+						<View style={styles.mainView}>
+							<Text style={styles.noDataText}>No Data Available</Text>
+						</View>
+						:
+						<ToDoList
+							items={this.state.items}
+							onPressItem={this.openItem}
+							onLongPressItem={this.alertMenu} />
+				}
+
+				<TouchableHighlight
+					style={[styles.button, styles.newButton]}
+					underlayColor='#99d9f4'
+					onPress={this.openItem}>
+					<Text style={styles.buttonText}>+</Text>
+				</TouchableHighlight>
+			</View>
+		);
+	}
 }
 
 export default ToDoContainer;
